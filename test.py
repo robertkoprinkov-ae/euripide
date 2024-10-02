903ab5c63cd95cbbaa633f41d347e42ee80d6fe6
Robert <robertkoprinkov@gmail.com>
2024-10-02 23:13:10 +0200

    [Minor] constrained run setup slightly changed

--- test.py
+++ test.py
@@ -2,29 +2,24 @@ import src.BoundedGP as BoundedGP
 import numpy as np
 import src.vis as vis
 
-
 lowerbound = -10.
 upperbound = 20.
 
-# Gaussian kernel
-def k(x, x_):
-    sigma = 25.
-    theta = 0.2
-    return np.power(sigma, 2.) * np.exp(-np.power(x-x_, 2.)/(2*np.power(theta, 2.)))
-
 interpol_x, interpol_y = np.array([0.0, 0.1, 0.19, 0.41, 0.52, 0.9]), np.array([10.0, 8., -9., -6., 12., 17.])
+
+kernel = BoundedGP.gaussian_kernel(params={'sigma': 25., 'theta': 0.2})
 
 # this is going to be a problem, the knots. We will need an adaptive approach because we don't know a priori
 # how the knots will be distributed. Maybe even something like multigrid.
-boundedGP = BoundedGP.BoundedGP(50, 0., 1., k, interpol_x, interpol_y, lowerbound=lowerbound, upperbound=upperbound)
+boundedGP = BoundedGP.BoundedGP(50, 0., 1., kernel, interpol_x, interpol_y, lowerbound=lowerbound, upperbound=upperbound)
 
 mean_knots, var_knots = boundedGP.statistics_prior()
-
 #sample = boundedGP.sample_interpolation(sample_pts)
 
 mean, var = boundedGP.statistics_interpolation()
 
 visualize = vis.Visualize()
 
+visualize.visualize_optim_methods(boundedGP)
 visualize.plot_interpolated(boundedGP)
 visualize.plot_constrained(boundedGP)
